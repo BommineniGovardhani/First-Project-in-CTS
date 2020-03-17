f3daa9b5cd03585109fd2b2c1e2a30b57278ffe0
BommineniGovardhani <59440130+BommineniGovardhani@users.noreply.github.com>
2020-03-17 11:15:25 +0530

    Add files via upload

--- Sample.java
+++ Sample.java
@@ -1,58 +1,20 @@
+package demo;
 
-
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class Sample
- */
-public class Sample extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Sample() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+public class Sample {
+	int sid;
+	String name;
+	public int getSid() {
+		return sid;
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		PrintWriter out=response.getWriter();
-		//out.println("<body style='background_color:blue;'>");
-		out.println("<body bgcolor='lightpink'>");
-		String user=request.getParameter("u1");
-		String pass=request.getParameter("p1");
-		String gen1=request.getParameter("r1");
-		out.println("<h2>Username  :"+user+"        Password  :"+pass+""+"          Gender  :"+gen1);
-		String[] lang1=request.getParameterValues("c1");
-		out.println("<h2>Language");
-		for(int i=0;i<lang1.length;i++)
-		{
-			out.println(lang1[i]);
-		}
-		String[] lang2=request.getParameterValues("s1");
-		for(int i=0;i<lang2.length;i++)
-		{
-			out.println("<h2>Qualifiication: "+lang2[i]);
-		}
+	public void setSid(int sid) {
+		this.sid = sid;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 
 }
